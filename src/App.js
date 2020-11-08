ad77553f0e2dd4038a385533978ea1c31e24714c
chokrihamza <chokrihamza1994@gmail.com>
2020-11-08 15:06:59 +0200

    update branch hamzafgg

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1>hello aymen</h1>
+
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h1>hello aymen</h1>
 
           Learn React
-        </a>
-      </header>
-    </div>
+    </a>
+
   );
 }
 
